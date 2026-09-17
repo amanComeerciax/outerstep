e87040d0cbb15f681f3ff5de8563c8296d2c74c7
commerciax <commerciax@commerciaxs-MacBook-Pro.local>
2026-09-17 11:54:12 +0530

    feat: remove footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ToolsShowcase from "@/components/ToolsShowcase";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#0b3536] text-white flex flex-col justify-between">
+    <main className="min-h-screen bg-[#0b3536] text-white">
       <ToolsShowcase />
-      <Footer />
     </main>
   );
 }
